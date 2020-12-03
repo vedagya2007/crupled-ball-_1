343640b535972018cc62e10e9452a7842f116f4f
vedagya2007 <74814960+vedagya2007@users.noreply.github.com>
2020-12-03 11:36:43 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,20 @@
+class Paper {  
+    constructor(x, y, radius){
+        var options = {
+            'restitution':0.3,
+            'friction':0.5,
+            'density':1.2,
+            'isStatic' : false
+        }
+         this.radius = radius
+        this.body = Bodies.circle(x, y, radius, options);
+	    World.add(world, this.body);
+       
+        
+   }
+   display(){
+    fill("red");
+    ellipseMode(RADIUS);
+    ellipse(this.body.position.x, this.body.position.y, this.radius);
+   }
+}
